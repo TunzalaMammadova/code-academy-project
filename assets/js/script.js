98a421df5b320be5790b9eb8e83e9674b66a0016
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-03-09 00:51:46 +0400

    added project file end version

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,23 +17,39 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+
 let academyList = document.querySelector(".academy");
 let educationList = document.querySelector(".education");
 let academyPage = document.querySelector(".academy-page");
 let educationPage = document.querySelector(".education-page");
+let corporatePage = document.querySelector(".corporate");
+let contactPage = document.querySelector(".contact");
+let sliderPage = document.querySelector(".slider");
+
 
 academyList.addEventListener("mouseover", function () {
     academyPage.classList.remove("d-none");
-})
-
-academyList.addEventListener("mouseout", function () {
-    academyPage.classList.add("d-none");
+    educationPage.classList.add("d-none");
 })
 
 educationList.addEventListener("mouseover", function () {
     educationPage.classList.remove("d-none");
+    academyPage.classList.add("d-none");
 })
 
-educationList.addEventListener("mouseout", function () {
+sliderPage.addEventListener("mouseover", function () {
+    academyPage.classList.add("d-none");
     educationPage.classList.add("d-none");
 })
+
+contactPage.addEventListener("mouseover", function () {
+    academyPage.classList.add("d-none");
+    educationPage.classList.add("d-none");
+})
+
+corporatePage.addEventListener("mouseover", function () {
+    academyPage.classList.add("d-none");
+    educationPage.classList.add("d-none");
+})
+
+
